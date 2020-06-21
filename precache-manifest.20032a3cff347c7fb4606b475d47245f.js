0560a58b8b8cf9a1ea3bd22f476fef459acc4a18
yao <45513771+yaox023@users.noreply.github.com>
2020-06-21 20:55:18 +0800

    a

---
+++ precache-manifest.20032a3cff347c7fb4606b475d47245f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c443c1344970e8e64020ca290ac06d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1134e3e34b2eb7d20dc",
+    "url": "/static/css/main.841c953d.chunk.css"
+  },
+  {
+    "revision": "a8a1ff56263d96437d36",
+    "url": "/static/js/2.9b71740d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b71740d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1134e3e34b2eb7d20dc",
+    "url": "/static/js/main.5f7e0d31.chunk.js"
+  },
+  {
+    "revision": "6af9ef12ae74a66ad6f9",
+    "url": "/static/js/runtime-main.69773362.js"
+  }
+]);
